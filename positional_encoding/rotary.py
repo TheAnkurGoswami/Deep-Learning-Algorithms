1b661c83dd47b1e4cea9bc66ba8c17ad9946a0f2
अंkur गोswami <kinglusifer1401@gmail.com>
2025-08-09 00:26:38 +0530

    improvments in code

--- positional_encoding/rotary.py
+++ positional_encoding/rotary.py
@@ -7,26 +7,21 @@ class RotaryPositionalEncoding(torch.nn.Module):
         self.wavelength = 10_000
         self.d_model = d_model
 
-    def get_rotation_matrix(self, seq_len: int) -> torch.Tensor:
-        ix = torch.arange(0, self.d_model) // 2
+        ix = torch.arange(self.d_model) // 2
         # ix -> [0, 0, 1, 1, .... d/2 - 1, d/2 - 1]
-        theta = self.wavelength ** (-2 * (ix) / self.d_model)
-        m = torch.arange(0, seq_len).view(-1, 1)
-        return theta * m
-        # theta * m -> (seq_len, d_model)
+        self.theta = self.wavelength ** (-2 * ix / self.d_model)
 
     def forward(self, input_: torch.Tensor) -> torch.Tensor:
         # input_ -> (batch_size, seq_len, d_model)
-        first_comp = input_
+        seq_len = input_.shape[1]
+        m = torch.arange(seq_len).view(-1, 1)
+        theta_m = self.theta * m
+        # theta_m -> (seq_len, d_model)
+
+        first_comp = input_ * torch.cos(theta_m)
         second_comp = torch.stack(
             (-input_[:, :, 1::2], input_[:, :, ::2]), dim=-1
-        ).reshape(input_.shape)
+        ).reshape(input_.shape) * torch.sin(theta_m)
 
-        rotation_mat = self.get_rotation_matrix(seq_len=input_.shape[1])
-
-        rotated_pe = first_comp * torch.cos(
-            rotation_mat
-        ) + second_comp * torch.sin(rotation_mat)
-
-        return rotated_pe
-        # rotated_pe -> (batch_size, seq_len, d_model)
+        return first_comp + second_comp
+        # (batch_size, seq_len, d_model)
